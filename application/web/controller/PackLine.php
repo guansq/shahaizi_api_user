ac3352aec169518bd3e02051e383a5a6420d818b
will <314112362@qq.com>
2017-09-21 18:46:54 +0800

    update web

--- application/web/controller/PackLine.php
+++ application/web/controller/PackLine.php
@@ -3,6 +3,7 @@
 namespace app\web\controller;
 
 use app\common\logic\PackLineLogic;
+use app\common\logic\SellerLogic;
 use think\Request;
 
 class PackLine extends WebBase{
@@ -11,12 +12,17 @@ class PackLine extends WebBase{
     public function detail(Request $request){
         $id = input('id');
         $lineLogic = new PackLineLogic();
+        $sellerLogic = new SellerLogic();
         $line = $lineLogic->find($id);
         if(empty($line)){
             return $this->error('你要查看的路线已经不存在');
         }
+        $driver =$sellerLogic->find($line['seller_id']);
+
         $line = $line->toArray();
+        $driver = $driver->toArray();
         $this->assign('line',$line);
+        $this->assign('driver',$driver);
         return $this->fetch('detail');
     }
 
