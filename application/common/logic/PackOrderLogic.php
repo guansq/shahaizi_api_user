d2538da09a48be32b649d6c6227a9ad6010e73a7
will <314112362@qq.com>
2017-09-23 17:01:29 +0800

    update 订单详情 优化

--- application/common/logic/PackOrderLogic.php
+++ application/common/logic/PackOrderLogic.php
@@ -90,17 +90,20 @@ class PackOrderLogic extends Model{
                 'msg' => '数据为空'
             ];
         }else{
+
+            $seller = $sellerLogic->find($info['seller_id']);
+            $carBar = $carBarLogic->find($info['con_car_type']);
+            $info['con_car_type_name'] =  empty($carBar['car_info'])?'':$carBar['car_info'];
+            $carBar = $carBarLogic->find($info['req_car_type']);
+            $info['req_car_type'] = empty($carBar['car_info'])?'':$carBar['car_info'];
+            $info['hx_user_name'] = empty($seller['hx_user_name'])?'':$seller['hx_user_name'];
+            $info['nickname'] =  empty($seller['nickname'])?'':$seller['nickname'];
+            $info['avatar'] =  empty($seller['head_pic'])?'':$seller['head_pic'];
             $return = [
                 'status' => 1,
                 'msg' => '成功',
                 'result' => $info
             ];
-            $seller = $sellerLogic->find($info['seller_id']);
-            $carBar = $carBarLogic->find($info['con_car_type']);
-            $info['con_car_type_name'] = $carBar['car_info'];
-            $info['hx_user_name'] = empty($seller['hx_user_name'])?'':$seller['hx_user_name'];
-            $info['nickname'] =  empty($seller['nickname'])?'':$seller['nickname'];
-            $info['avatar'] =  empty($seller['head_pic'])?'':$seller['head_pic'];
         }
         return $return;
     }
