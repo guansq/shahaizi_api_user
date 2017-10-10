33071372a76a644f94f6675eae54c90b0ee7ea44
will <314112362@qq.com>
2017-10-10 17:31:35 +0800

    fix

--- application/web/config.php
+++ application/web/config.php
@@ -3,10 +3,15 @@ $home_config = [
     // +----------------------------------------------------------------------
     // | 模板设置
     // +----------------------------------------------------------------------
-	//默认错误跳转对应的模板文件
-	'dispatch_error_tmpl' => 'public:dispatch_jump',
-	//默认成功跳转对应的模板文件
-	'dispatch_success_tmpl' => 'public:dispatch_jump', 
+    //默认错误跳转对应的模板文件
+    'dispatch_error_tmpl' => 'public:dispatch_jump',
+    //默认成功跳转对应的模板文件
+    'dispatch_success_tmpl' => 'public:dispatch_jump',
+
+    // 默认输出类型
+    'default_return_type' => 'html',
 ];
 
+
+return $home_config;
 ?>
