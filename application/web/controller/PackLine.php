43fd6977cb2067ac1253cee10f2be77b8512892b
will <314112362@qq.com>
2017-10-18 18:07:31 +0800

    update web/packLine/detail

--- application/web/controller/PackLine.php
+++ application/web/controller/PackLine.php
@@ -2,6 +2,7 @@
 
 namespace app\web\controller;
 
+use app\common\logic\OrderCommentLogic;
 use app\common\logic\PackCommentLogic;
 use app\common\logic\PackLineLogic;
 use app\common\logic\SellerLogic;
@@ -14,23 +15,35 @@ class PackLine extends WebBase{
         $id = input('id');
         $lineLogic = new PackLineLogic();
         $sellerLogic = new SellerLogic();
-        $lineCommentLogic = new PackCommentLogic();
+        $orderCommentLogic = new OrderCommentLogic();
         $line = $lineLogic->find($id);
         if(empty($line)){
             return $this->error('你要查看的路线已经不存在');
         }
+
+
         $line = $line->toArray();
         $line_detail=$line['line_detail'];
         $de_json=html_json($line_detail);
         $line['line_detail'] =object_to_array($de_json);
-        $line['plat_start'] = $lineCommentLogic->getStartBylineId($id);
+        $where =[
+            'line_id'=>$id,
+            'deleted'=>0
+        ];
+        $line['score'] = intval($orderCommentLogic->where($where)->avg('pack_order_score'));
         $line['line_price_fmt'] = moneyFormat($line['line_price']);
         $driverInfo = $sellerLogic->getInfoById($line['seller_id']);
-        $comment = $lineCommentLogic->getCommentPageBylineId($line['line_id'],PHP_INT_MAX);
         //dd($line['line_detail'][0]);  //todo 添加数据校验
         $this->assign('line', $line);
         $this->assign('driver', $driverInfo);
-        $this->assign('comment', $comment);
+
+        $commentLogic = new OrderCommentLogic();
+        $list = $commentLogic->getListByWere($where);
+        $comments =[
+            'total'=>count($list),
+            'list'=>$list,
+        ];
+        $this->assign('comments',$comments);
         return $this->fetch();
     }
 
