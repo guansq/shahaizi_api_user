903b0fd028c8529d101806f75cf88724f5ba9e8c
GuanShaoQiu <GuanShaoQiu@ruitukeji.com>
2017-10-24 17:59:05 +0800

    fix bug

---
+++ application/common/logic/SystemMessageLogic.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/10/24
+ * Time: 16:52
+ */
+namespace app\common\logic;
+
+use think\Model;
+use think\Db;
+
+class SystemMessageLogic extends BaseLogic
+{
+
+}
