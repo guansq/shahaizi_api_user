d52ea446880deb3e4866ac73566fd7f06336e1d4
GuanShaoQiu <GuanShaoQiu@ruitukeji.com>
2017-11-02 15:50:07 +0800

    帮助中心内容调用

--- application/web/controller/Help.php
+++ application/web/controller/Help.php
@@ -8,6 +8,12 @@ use think\Request;
 
 class Help extends WebBase{
     public function index(){
+        $list = M('help_content')->select();
+        foreach($list as &$val){
+            $val['content'] = htmlspecialchars_decode($val['content']);
+        }
+        $this->assign('list',$list);
+        //print_r($list);
         return $this->fetch('help-center');
      }
 }
