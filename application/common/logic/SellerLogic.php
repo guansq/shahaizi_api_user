c87a8175aa809c1ccf9f6f8ac0e03ecff8384cfe
GuanShaoQiu <GuanShaoQiu@ruitukeji.com>
2017-11-22 17:55:26 +0800

    fix bug

--- application/common/logic/SellerLogic.php
+++ application/common/logic/SellerLogic.php
@@ -79,7 +79,7 @@ class SellerLogic extends BaseLogic{
 
         $seller = $seller->toArray();
         $seller['countryName'] = $regCouLogic->where('id', $seller['countryId'])->value('name').'';
-        $seller['nickname'] = $isAnonymous == 1 ? '司导' : $seller['nickname'];
+        $seller['nickname'] = $isAnonymous == 1 ? firstStr($seller['nickname']) : $seller['nickname'];
         $seller['cityName'] = $regionLogic->where('id', $seller['city'])->value('name').'';
         $seller['platStart'] = intval($seller['platStart']);
         return $seller;
